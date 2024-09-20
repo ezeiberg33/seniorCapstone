b4150a8d4576491c7e9fe81c2f8bc2772e566a4f
ezeiberg33 <89863575+ezeiberg33@users.noreply.github.com>
2024-09-20 15:51:23 -0500

    Update constantSpeed.py

--- constantSpeed.py
+++ constantSpeed.py
@@ -1,54 +1,19 @@
-import math
-from board import SCL,SDA
-import busio
-from adafruit_pca9685 import PCA9685
-import time
-import adafruit_motor.servo
+#Reading of 0 means magnet detected,
+#reading of 1 means no magnet detected
+
 import RPi.GPIO as IO
-import rospy
-from std_msgs.msg import Float32MultiArray
-from std_msgs.msg import Float32
+import time
 import sys
 import argparse
+import busio
 import smbus
 from time import sleep
-import Adafruit_GPIO.SPI as SPI
-import Adafruit_MCP3008
-
-IO.setwarnings(False)
-IO.setmode(IO.BCM)
-
-pin_num = 26
-IO.setup(pin_num,IO.IN,IO.PUD_UP)
-
-start_time = time.time()
-timeLimit = 5
-samplePeriod = .5
-speeds = []
-n = 0
-
-def read():
-    gear_rotations = 0
-    time_start = time.time()
-    time_end = time.time()
-    ready_to_go = False
-    while gear_rotations < 5:
-        curr_pin_val = IO.input(pin_num)
-        if curr_pin_val == 1 and ready_to_go == False:
-            ready_to_go = True
-            gear_rotations += 1
-        if curr_pin_val == 0 and ready_to_go == True:
-            ready_to_go = False
-        if time.time() > time_start + 0.5:
-            return 0
-        #print(gear_rotations)
-    #print('hi!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
-    time_end = time.time()
-    #speed = (math.pi*.111 / (time_end - time_start)) / 1000
-    speed = (2*math.pi*.111) / (time_end - time_start)
-    time_start = time.time()
-    return speed
-
+import adafruit_mcp3xxx.mcp3008 as MCP
+from adafruit_mcp3xxx.analog_in import AnalogIn
+import math
+from board import SCL,SDA
+from adafruit_pca9685 import PCA9685
+import adafruit_motor.servo
 
 def Servo_Motor_Initialization():
    i2c_bus = busio.I2C(SCL,SDA)
@@ -72,27 +37,43 @@ def Motor_Speed(pca,percent):
    pca.channels[15].duty_cycle = math.floor(speed)
    print(speed/65535)
 
-#initialization
+runtime = 5
+magnet_detected = false
+start_time = time.time()
+times = []
+speeds = []
+prev_magnet_time = start_time
+new_magnet_time = 0
+distance = CHECK
+IO.setwarnings(False)
+IO.setmode(IO.BCM)
+
+GPIO_num = 16
+IO.setup(GPIO_num,IO.IN,IO.PUD_UP)
+
 pca = Servo_Motor_Initialization()
 Motor_Start(pca)
-Motor_Speed(pca, 0.15)   
+Motor_Speed(pca, 0.15)  
 
-while time.time()-start_time < timeLimit:
-  if time.time()-start_time > n*samplePeriod:
-    speeds.append(read())
-    n = n+1
+while time.time() - start_time < run_time:
+    curr_pin_val = IO.input(GPIO_num)
+    print(curr_pin_val)
+    if curr_pin_val = 0 and last_pin_val = 1:
+        new_magnet_time = datetime.datetime.now()
+        dt = new_magnet_time - prev_magnet_time
+        speeds.append(distance/dt)
+        times.append(new_magnet_time-start_time)
 
 Motor_Speed(pca, 0) 
-GPIO.cleanup(0)
 
-x = input("Press 1 to create speed vs. time figure")
-title = input('Enter filename for the figure (w/ .png)')
-
+x = input('Press 1 to create speed vs. time figure')
+title = input('Enter filename for the figure (w/ .png')
 if x == 1:
-  plt.clf()
-  time = np.linspace(0, samplePeriod, timeLimit)
-  plt.plot(time, speeds)
-  plt.grid(True)
-  plt.xlabel("Time (s)")
-  plt.ylabel("Speed")
-  plt.savefig(title)
+    plt.clf()
+    plt.plot(times, speeds)
+    plt.grid(True)
+    plt.xlabel('Time (s)')
+    plt.ylabel('Speed (m/s)')
+    plt.savefig(title)
+
+        
